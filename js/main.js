ac4bce388a1098ae2348af0498b0954e431dd7a5
JBidal <joelbidal1@hotmail.com>
2020-07-22 15:53:17 -0300

    agregando javascript

---
+++ js/main.js
@@ -0,0 +1,64 @@
+function show2(){
+    // Imagen de zapatilla principal
+document.getElementById('box-shoes').style.display="none";
+document.getElementById('box-shoes-2').style.display="block";
+document.getElementById('box-shoes-3').style.display="none";
+document.getElementById('box-shoes-4').style.display="none";
+    // Modelo de zapatilla
+document.getElementById('shoes-model').style.display="none";
+document.getElementById('shoes-model-2').style.display="block";
+document.getElementById('shoes-model-3').style.display="none";
+    // Descripción del modelo
+document.getElementById('description-shoes').style.display="none";
+document.getElementById('description-shoes-2').style.display="block";
+document.getElementById('description-shoes-3').style.display="none";
+document.getElementById('description-shoes-4').style.display="none";
+    // Precio
+document.getElementById('price').style.display="none";
+document.getElementById('price-2').style.display="block";
+document.getElementById('price-3').style.display="none";
+}
+
+
+function show3(){
+    // Imagen de zapatilla principal
+document.getElementById('box-shoes').style.display="none";
+document.getElementById('box-shoes-2').style.display="none";
+document.getElementById('box-shoes-3').style.display="block";
+document.getElementById('box-shoes-4').style.display="none";
+    // Modelo de zapatilla
+document.getElementById('shoes-model').style.display="none";
+document.getElementById('shoes-model-2').style.display="none";
+document.getElementById('shoes-model-3').style.display="block";
+    // Descripción del modelo
+document.getElementById('description-shoes').style.display="none";
+document.getElementById('description-shoes-2').style.display="none";
+document.getElementById('description-shoes-3').style.display="block";
+document.getElementById('description-shoes-4').style.display="none";
+    // Precio
+document.getElementById('price').style.display="none";
+document.getElementById('price-2').style.display="none";
+document.getElementById('price-3').style.display="block";
+}
+
+
+function show4(){
+    // Imagen de zapatilla principal
+document.getElementById('box-shoes').style.display="none";
+document.getElementById('box-shoes-2').style.display="none";
+document.getElementById('box-shoes-3').style.display="none";
+document.getElementById('box-shoes-4').style.display="block";
+    // Modelo de zapatilla
+document.getElementById('shoes-model').style.display="block";
+document.getElementById('shoes-model-2').style.display="none";
+document.getElementById('shoes-model-3').style.display="none";
+    // Descripción del modelo
+document.getElementById('description-shoes').style.display="none";
+document.getElementById('description-shoes-2').style.display="none";
+document.getElementById('description-shoes-3').style.display="none";
+document.getElementById('description-shoes-4').style.display="block";
+    // Precio
+document.getElementById('price').style.display="block";
+document.getElementById('price-2').style.display="none";
+document.getElementById('price-3').style.display="none";
+}
